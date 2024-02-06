0969afc609654aa89ec58e86ed8805091dc82160
NA-varez <nalvar95@outlook.com>
2024-02-06 13:52:16 -0800

    Lab 3 all done

--- lab3/main.hpp
+++ lab3/main.hpp
@@ -4,13 +4,15 @@
 *
 * Description: Practice header file
 *
-* Author: Nicolas Alvarez and Richard Rios
+* Author: Nicolas Alvarez
 *
 * Version: 0.1
 *
 **********************************************/
 #ifndef _MAIN_H
 #define _MAIN_H
+
+//Nothing here yet
 
 #endif //- _MAIN_H
 
