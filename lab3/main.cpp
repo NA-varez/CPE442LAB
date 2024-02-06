f9c8d5578458421c64adaba5ee6f9b853baaaf8e
NA-varez <nalvar95@outlook.com>
2024-02-06 13:07:15 -0800

    Lab3 fixed

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -4,12 +4,16 @@
 *
 * Description: Lab 3 main for sobel filter
 *				creates an executable that takes
-*				a video as input and output .....
+*				a video frame as input, converts the frame to grayscale
+				then converts the grayscale frame to sobel frames.
+				Outputs the frames in a new window.
 *
 * Author: Nicolas Alvarez
 *
 * Version: 0.1
 *
+* Reference: Richard Rios: Helped fix uchar overflow in sobel function
+*			 that was reducing quality of sobel output.
 **********************************************/
 #include <stdio.h>
 #include "sobel.hpp"
@@ -25,80 +29,6 @@
 using namespace cv;
 using namespace std;
 
-//
-//
-//
-void to442_grayscale(Mat& input, Mat& output) {
-	// Check if input and output have the same size
-    if (input.size() != output.size()) {
-        cerr << "Input and output Mat sizes do not match!" << endl;
-        return;
-    }
-
-    // Check if input is empty
-    if (input.empty()) {
-        cerr << "Input Mat is empty!" << endl;
-        return;
-    }
-	
-	//For each pixel in Mat
-	for (int i = 0; i < input.rows - 1; ++i) {
-		for (int j = 0; j < input.cols - 1; ++j) {
-			Vec3b pixel = input.at<Vec3b>(i, j);
-
-			//Red = pixel[2];
-			//Green = pixel[1];
-			//Blue = pixel[0];
-
-			//ITU-R (BT.709) recommended algorithm for grayscale
-			uchar grayPixel = (0.2126 * pixel[2] + 0.7152 * pixel[1] + 0.0722 * pixel[0]);
-			//All pixels now represent 1 'intensity' value that will be used in the sobel
-			output.at<uchar>(i, j) = grayPixel;
-		}	
-	}	
-}
-
-//
-//
-//
-void to442_sobel(Mat& input, Mat& output) {
-	
-	//For each pixel in Mat (except on border)
-	//Starts sobel in row 1 column 1 (inclusive 0)
-	for (int i = 1; i < input.rows - 1; ++i) {
-		for (int j = 1; j < input.cols - 1; ++j) {
-
-			//X and Y filter operations on surrounding intensity pixels
-			uchar g_x = (-1*input.at<uchar>(i-1,j-1)) + input.at<uchar>(i-1,j+1) +
-					(-2*input.at<uchar>(i,j-1)) + 2*input.at<uchar>(i,j+1) +
-					(-1*input.at<uchar>(i+1,j-1)) + input.at<uchar>(i+1,j+1);
-			uchar g_y = (-1*input.at<uchar>(i-1,j-1)) + -1*input.at<uchar>(i-1,j+1) +
-					(-2*input.at<uchar>(i-1,j)) + 2*input.at<uchar>(i+1,j) +
-					(1*input.at<uchar>(i+1,j-1)) + 1*input.at<uchar>(i+1,j+1);
-			
-
-			//Approximation of Sobel without using pow or sqrt
-			output.at<uchar>(i, j) = std::abs(g_x) + std::abs(g_y);
-		}
-		
-	}
-
-	//Pad top and bottom border pixels as zero
-	for(int i = 0; i < input.cols; ++i) {
-		//First row
-		input.at<uchar>(0, i) = 0;
-		//Last row
-		input.at<uchar>(input.rows - 1, i) = 0;
-	}
-
-	//Pad left and right border pixels as zero
-	for(int j = 0; j < input.rows; ++j) {
-		//First column
-		input.at<uchar>(j, 0) = 0;
-		//Last column
-		input.at<uchar>(j, input.cols - 1) = 0;
-	}
-}
 
 int main(int argc, char** argv) {
     
--- lab3/sobel.hpp
+++ lab3/sobel.hpp
@@ -12,6 +12,8 @@
 #ifndef _SOBEL_H
 #define _SOBEL_H
 
-
+//Function prototypes
+void to442_grayscale(Mat& input, Mat& output);
+void to442_sobel(Mat& input, Mat& output);
 
 #endif //- _SOBEL_H
