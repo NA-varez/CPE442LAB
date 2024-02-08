157fe115f6c0fa329fa30071dbacb1be354fa192
NA-varez <nalvar95@outlook.com>
2024-02-08 10:31:55 -0800

    new video and start of lab 4

---
+++ lab4/main.hpp
@@ -0,0 +1,18 @@
+/*********************************************
+*
+* File: main.hpp
+*
+* Description: Practice header file
+*
+* Author: Nicolas Alvarez
+*
+* Version: 0.1
+*
+**********************************************/
+#ifndef _MAIN_H
+#define _MAIN_H
+
+//Nothing here yet
+
+#endif //- _MAIN_H
+
